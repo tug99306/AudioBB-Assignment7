f0a2fc9648a211b65b51a6d365b6c2d9838326c1
Saleem Hussein <tug99306@temple.edu>
2021-10-26 22:14:11 -0400

    Made BookAdapter.kt changed layout files around

---
+++ app/src/main/java/edu/temple/audiobb/BookAdapter.kt
@@ -0,0 +1,38 @@
+package edu.temple.audiobb
+
+import android.content.Context
+import android.text.Layout
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.widget.TextView
+
+class BookAdapter(_context: Context, var _bookList:BookList, val clickListener : (bookInt: Int) -> Unit) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+
+    private val inflater = LayoutInflater.from(_context)
+
+    class BookViewHolder(itemView: View, val clickListener : (bookInt:Int) -> Unit) :  RecyclerView.ViewHolder(itemView){
+        val bookTitleView : TextView = itemView.findViewById(R.id.bookTitleView)
+        val bookAuthorView : TextView = itemView.findViewById(R.id.bookAuthorView)
+
+        init {
+            itemView.setOnClickListener {
+                clickListener(adapterPosition)
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookAdapter.BookViewHolder {
+        var view = inflater.inflate(R.layout.bookrecycler, null)
+        return BookViewHolder(view, clickListener)
+    }
+
+    override fun onBindViewHolder(holder: BookAdapter.BookViewHolder, position: Int) {
+        holder.bookTitleView.text = _bookList.getBook(position).title
+        holder.bookAuthorView.text = _bookList.getBook(position).author
+    }
+
+    override fun getItemCount(): Int {
+        return _bookList.getSize()
+    }
+}
