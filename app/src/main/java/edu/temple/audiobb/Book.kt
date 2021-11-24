cf044e80b0b9fccdb2f937021e26ee8f829575b0
Saleem Hussein <tug99306@temple.edu>
2021-11-24 03:13:17 -0500

    Add duration param

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -1,4 +1,4 @@
 package edu.temple.audiobb
 import java.io.Serializable
 
-data class Book (val id : Int, val title : String, val author : String,  val cover_url : String): Serializable
+data class Book (val id : Int, val title : String, val author : String,  val cover_url : String, val duration: Int): Serializable
