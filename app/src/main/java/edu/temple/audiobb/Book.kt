006ff45f2b5837451670577985132bc91236dcc1
Saleem Hussein <tug99306@temple.edu>
2021-12-05 15:00:00 -0500

    Update to proper json implementation

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -1,4 +1,9 @@
 package edu.temple.audiobb
+import org.json.JSONObject
 import java.io.Serializable
 
-data class Book (val id : Int, val title : String, val author : String,  val cover_url : String, val duration: Int): Serializable
+data class Book (val id : Int, val title : String, val author : String, val duration: Int, val cover_url : String) : Serializable {
+constructor(book: JSONObject) :
+        this(book.getInt("id"), book.getString("title"), book.getString("author"),
+        book.getInt("duration"), book.getString("cover_url"))
+}
